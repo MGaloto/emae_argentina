cfabdd99b6a36e6cf036ae4677b7b715f9e0e647
MGaloto <maxigaloto@hotmail.com>
2023-03-04 16:36:34 -0300

    cambios en docker environment

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -3,9 +3,6 @@
 echo "Build the docker"
 
 docker build . --progress=plain \
-               --build-arg QUARTO_VERSION=1.1.149 \
-               --build-arg CONDA_ENV=flex_dashboard \
-               --build-arg PYTHON_VER=3.8 \
                -t mgaloto/flexdashiny_01
 
 if [[ $? = 0 ]] ; then
